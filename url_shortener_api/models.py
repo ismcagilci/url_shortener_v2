62e6579e8ed522e34c1814106a5b1a11ef99c239
ismailcagilci <ismcagilci@gmail.com>
2024-02-14 16:13:55 +0300

    final-commit

--- url_shortener_api/models.py
+++ url_shortener_api/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class ShortenedURL(models.Model):
-    original_url = models.URLField(max_length=700)
-    shortened_url_value = models.CharField(max_length=10, null=True, unique=True)
+    original_url = models.URLField(max_length=200)
+    shortened_url_value = models.CharField(max_length=6, null=True, unique=True)
 
     def __str__(self):
         return self.original_url
